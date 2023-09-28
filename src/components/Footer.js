e2237ef0a331aa27257cb63ba2777ce99980f7e3
devrupani <drupani01@gmail.com>
2023-09-29 04:50:08 +0530

    abc

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,19 @@ function Footer() {
     return (
         <>
             <div className="container">
-                <img src="../logo.png" width={185} alt="Certificate Gen" />
+                <div className="row">
+                    <div className="col-4">
+                        <img src="../logo.png" width={185} alt="Certificate Gen" />
+                    </div>
+                    <div className="col-8">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Templates</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div >
         </>
     );
